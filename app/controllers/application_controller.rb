cce9c2afb07d9ea988154a5c0c942d1ff46cbae6
Krystle Frisbie <Stangcutie723@gmail.com>
2013-07-13 13:49:13 -0700

    Implement delete functionality for projects

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,16 @@ end
 class ProjectsController < ApplicationController
   def index
   end
+
+  private
+    def find_project
+      before_filter :find_project, :only => [:show,
+                                            :edit,
+                                            :update,
+                                            :destroy]
+     rescue ActiveRecord::RecordNotFound
+     flash[:alert] = "The project you were looking" +
+      " for could not be found."
+      redirect_to projects_path
+   end
 end

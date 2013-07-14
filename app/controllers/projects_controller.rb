a79367bba28d118c8dcc42584e6dcfc844b6a439
Krystle Frisbie <Stangcutie723@gmail.com>
2013-07-13 17:34:44 -0700

    Redirect the users back to the projects page if they try going to a project that doesn’t exist.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,49 +1,53 @@
 class ProjectsController < ApplicationController
   def index
     @projects = Project.all
+
   end
 
   def new
     @project = Project.new
   end
-end
 
-def create
-  @project = Project.new(params[:project]) <co id="ch03_551_1"/>
+  def create
+    @project = Project.new(params[:project]) <co id="ch03_551_1"/>
     if @project.save
       flash[:notice] = "Project has been created."
       redirect_to @project,
       :notice => "Project has been created."
-
     else
 # nothing, yet
     end
-end
-
-def show
-  @project = Project.find(params[:id])
-end
-
-def edit
-  @project = Project.find(params[:id])
-end
-
-def update
-  @project = Project.find(params[:id])
-  if @project.update_attributes(params[:project])
-    flash[:notice] = "Project has been updated."
-    redirect_to @project
-  else
-    flash[:alert] = "Project has not been updated."
-    render :action => "edit"
   end
-end
 
+  def show
+  end
 
+  def edit
+  end
 
-def destroy
-  @project = Project.find(params[:id])
-  @project.destroy
-  flash[:notice] = "Project has been deleted."
-  redirect_to projects_path
+  def update
+    if @project.update_attributes(params[:project])
+      flash[:notice] = "Project has been updated."
+      redirect_to @project
+    else
+      flash[:alert] = "Project has not been updated."
+      render :action => "edit"
+    end
+  end
+
+  def destroy
+    @project.destroy
+      flash[:notice] = "Project has been deleted."
+      :redirect_to projects_path
+  end
+
+private
+  def find_project
+    @project = Project.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+    flash[:alert] = "The project you were looking" +
+" for could not be found."
+    redirect_to projects_path
+  end
+before_filter :find_project, :only => [:show, :edit, :update, :destroy]
 end

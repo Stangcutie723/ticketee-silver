03288c0ecd8239d53cde886abf6bc66b3fd52dc6
Krystle Frisbie <Stangcutie723@gmail.com>
2013-07-16 11:18:10 -0700

    Add feature for ensuring that a user is signed in via confirmation email by using email spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,13 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :encryptable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
 
-  devise :database_authenticatable, :registerable,
-        :recoverable, :rememberable, :trackable, :validatable
-        <co id="_432_4125_3722_1"/>
+  devise  :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :trackable, :validatable,
+          :confirmable
+
+  #<co id="_432_4125_3722_1"/>
   # Setup accessible (or protected) attributes for your model
-    attr_accessible :email, :password,
-                    :password_confirmation, :remember_me
+  attr_accessible :email, :password,
+                  :password_confirmation, :remember_me
   # attr_accessible :title, :body
 end

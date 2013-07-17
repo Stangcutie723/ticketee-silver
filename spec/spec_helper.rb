3b3b1b1b04ae886538c55bf49b6ab7b67f12ed51
Krystle Frisbie <Stangcutie723@gmail.com>
2013-07-17 13:06:23 -0700

    simple cov addition

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,21 @@
 require 'simplecov'
-  SimpleCov.start
+SimpleCov.start 'rails'
 
-if ENV['RAILS_ENV'] == 'test'
-  require 'simplecov'
-  SimpleCov.start 'rails'
-  puts "required simplecov"
-end
+ENV['RAILS_ENV'] == 'test'
+require File.expand_path("../../config/environment",__ FILE__)
+require 'rspec/rails'
+require 'rspec/autorun'
 
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 
 RSpec.configure do |config|
   config.before do
-      ActionMailer::Base.deliveries.clear
-  config.include Devise::TestHelpers, :type => :controller
+    ActionMailer::Base.deliveries.clear
+  end
+
   config.include EmailSpec::Helper
+  config.include Devise::TestHelpers, :type => :controller
+
+  config.fixture_path = "#{::Rails.root}/spec/fixures"
 end
